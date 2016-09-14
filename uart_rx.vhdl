0575d1a42f437604f146a8ddf7737286c6bdc879
Matthias <mail@matthiasbock.net>
2016-09-14 15:03:24 +0200

    further decreased CTS wait time

--- uart_rx.vhdl
+++ uart_rx.vhdl
@@ -122,7 +122,7 @@ begin
                         uart_rx_data_ready <= '1';
                         -- higher functions get some time to process received data until line is cleared for next byte
                         bit_counter := 10;
-                    elsif (bit_counter < 100)
+                    elsif (bit_counter < 11)
                     then
                         bit_counter := bit_counter + 1;
                         synchronized := false;
